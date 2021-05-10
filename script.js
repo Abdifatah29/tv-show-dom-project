5c9a84674627f8604f4b81f4556bf383ebce72e6
Abdifatah29 <abdi.hassannoor@onedayonly.co.za>
2021-05-10 23:29:18 +0200

    removed episodes.js file and rendered content

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-//You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
-}
+$.ajax({
+    url: "https://api.tvmaze.com/shows/82/episodes",
+    type: "GET"
+}).done((data) => {
+    render(data);
+});
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+function render(episodes) {
+    for(let episode in episodes){
+        console.log(episodes[episode]['summary']);
+        $('#root').append("<div class='edisodeBlock'>"
+                + "<div><h3>" + episodes[episode]['name'] + "</h3></div>"
+                + "<div><img alt='asd' src='" + episodes[episode]['image']['medium'] + "'>"
+                +   "<p>" + episodes[episode]['summary'] + "</p>"
+                + "</div>"
+            + "</div>"
+        );
+    }
 }
-
-window.onload = setup;
